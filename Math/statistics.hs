2b1171ed24c767dcbd5dcf2aca05a45e0d70e249
Paul Ennemoser <tick@federrot.at>
2012-08-09 22:48:08 +0200

    Some gaussian functions

--- Math/statistics.hs
+++ Math/statistics.hs
@@ -19,9 +19,21 @@ stddev = sqrt . variance
 stdscore :: RealFloat a => a -> [a] -> a
 stdscore x xs = (x - mean xs) / stddev xs
 
+factorial :: Int -> Int
+factorial n = product [1..n]
+
 -- The number of ways to choose k elements from an n-element set.
 binorm :: Int -> Int -> Int
 binorm n k = product [k+1..n] `div` product [1..n-k]
 
-factorial :: Int -> Int
-factorial n = product [1..n]
+binormProb :: Floating a => Int -> Int -> a -> a
+binormProb n k p = fromIntegral (binorm n k) * (p ** fromIntegral k) * ((1.0 - p) ** fromIntegral (n-k))
+
+-- Probability density function
+probDensity :: Floating a => a -> a
+probDensity x = factor * exp (-0.5 * x * x)
+                where factor = 1.0 / (sqrt (2.0 * pi))
+
+-- Gaussian function
+gaussian :: Floating a => a -> a -> a -> a
+gaussian x mu sigma = 1.0 / sigma * probDensity ((x - mu) / sigma)
