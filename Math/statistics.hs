dc9163c51a8288be5d6d64a5035bd6c2739d69fc
Paul Ennemoser <tick@federrot.at>
2012-09-01 22:10:33 +0200

    Added covaraiance, correlation, slope and intercept of two variables

--- Math/statistics.hs
+++ Math/statistics.hs
@@ -10,6 +10,7 @@ median xs = sort xs !! (length xs `div` 2)
 mode :: Ord a => [a] -> a
 mode xs = head . last $ sortBy (compare `on` length) (groupBy (==) xs)
 
+-- The variance is a measure of how far a set of numbers is spread out
 variance :: (RealFrac a, Fractional b) => [a] -> b
 variance xs = mean $ map ((^2) . subtract (mean xs)) xs
 
@@ -26,6 +27,7 @@ factorial n = product [1..n]
 binorm :: Int -> Int -> Int
 binorm n k = product [k+1..n] `div` product [1..n-k]
 
+-- The probability for k events of chance p to occur at the same time in an n-element set.
 binormProb :: Floating a => Int -> Int -> a -> a
 binormProb n k p = fromIntegral (binorm n k) * (p ** fromIntegral k) * ((1.0 - p) ** fromIntegral (n-k))
 
@@ -37,3 +39,22 @@ probDensity x = factor * exp (-0.5 * x * x)
 -- Gaussian function
 gaussian :: Floating a => a -> a -> a -> a
 gaussian x mu sigma = 1.0 / sigma * probDensity ((x - mu) / sigma)
+
+-- Measures how much two random variables change together.
+-- covariance xs xs = variance xs
+covariance :: (RealFrac a, Fractional a) => [a] -> [a] -> a
+covariance xs ys = mean $ map distance (zip xs ys)
+                   where xmu = mean xs
+                         ymu = mean ys
+                         distance (x, y) = (x-xmu)*(y-ymu)
+
+-- Pearson's product-moment coefficient r                       
+correlation xs ys = (covariance xs ys) / stddev(xs) * stddev(ys)
+          
+-- Gets the slope b of the linear regression line through the data.
+slope xs ys = (covariance xs ys) / variance ys
+
+-- Gets the intercept a of the linear regression line through the data.
+intercept xs ys = mean ys - (b * mean xs) 
+                  where b = slope xs ys
+		  
